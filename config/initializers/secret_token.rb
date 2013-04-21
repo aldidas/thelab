d646b528e28d2514470ec78eaf0218576ec6c786
Aldi Daswanto <aldi@daswanto.com>
2013-04-21 18:57:44 +0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thelab::Application.config.secret_token = '8ea9c8e99d57c5276e294ee97a44e6a6b648547a7adc644580eaf13dfefbfbe64f242035df0e7ff801c386fece0333295dd7a79ca0c117cdfe747344e6441d8d'
