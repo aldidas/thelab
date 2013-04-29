560cc71966122cc7f1ab0488ccc251999ee96f99
Aldi Daswanto <aldi@daswanto.com>
2013-04-29 21:02:48 +0700

    Add Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Thelab::Application.routes.draw do
+  get 'about' => 'pages#about'
+
+  root :to => 'pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
